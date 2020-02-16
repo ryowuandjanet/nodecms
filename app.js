9f8d652cc5dd8f72a842d67e847df023878cea35
ryowuandjanet <ryowudata@gmail.com>
2020-02-16 12:45:04 +0800

    chapter 11

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/cms',{ useNewUrlParser: true, useUnifiedTopology: true}).then(db =>{
+	console.log("MONGO Connected");
+}).catch(error => console.log(error));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars',exphbs({defalutLayout:'home'}));
+const {select} = require('./helpers/handlebars-helpers');
+app.engine('handlebars',exphbs({defalutLayout:'home', helpers: {select: select}}));
 app.set('view engine','handlebars');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(methodOverride('_method'));
 
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
 
 app.use('/',home);
 app.use('/admin',admin);
+app.use('/admin/posts',posts);
 
 
 app.listen(3000, ()=>{
